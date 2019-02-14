6dae9b30f40fb4eb5a00cfc9ada436a08e603e43
Evgenii Zaets <evgeniizaets@gmail.com>
2019-02-14 19:03:06 +0200

    cors; set response format as json

--- api/controllers/ProductController.php
+++ api/controllers/ProductController.php
@@ -1,11 +1,29 @@
 <?php
 namespace api\controllers;
 
-use common\models\Product;
 use Yii;
 use yii\rest\ActiveController;
+use yii\web\Response;
+use yii\filters\Cors;
 
 class ProductController extends ActiveController
 {
     public $modelClass = 'common\models\Product';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+        ];
+
+        // указываем что ответ будет возвращаться только в json
+        $behaviors['contentNegotiator']['formats'] = [
+            'application/json' => Response::FORMAT_JSON
+        ];
+
+        return $behaviors;
+    }
 }
